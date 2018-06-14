c01d6bffe903b29b535e85ef2a9642e81685a04b
mebenstein-tgm <mebenstein@student.tgm.ac.at>
2018-06-14 10:33:57 +0200

    d

--- src/main/java/ostbahn/Repository/ZugRepository.java
+++ src/main/java/ostbahn/Repository/ZugRepository.java
@@ -1,7 +1,6 @@
 package ostbahn.Repository;
 
 
-import com.sun.xml.internal.bind.v2.model.core.ID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
